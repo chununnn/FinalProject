c8d843882dbac9499f094c9b51e479a4f6a8ee3b
chununnn <F74126042@gs.ncku.edu.tw>
2024-06-07 23:50:46 +0800

    TODO max

---
+++ test.java
@@ -0,0 +1,12 @@
+public class test {
+    public static void main(String[] args) {
+        System.out.println(max(1,2));
+    }
+
+    public static int max(int a, int b){
+        
+        //@return max(a,b);
+        
+        return 0;
+    }
+}
