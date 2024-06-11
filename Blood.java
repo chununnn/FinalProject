6b749cd9785cba50a7d0c017bf105692f9ce4e9b
chununnn <F74126042@gs.ncku.edu.tw>
2024-06-11 18:09:13 +0800

    add display blood and minus blood

--- Blood.java
+++ Blood.java
@@ -1,13 +1,45 @@
-public class Blood {
+import javax.swing.*;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.event.*;
+
+class Blood {
     private int blood;
-    Blood(int init_blood){  //初始化血量
-        this.blood = init_blood;
+    private Color color;
+    private int x;
+    private int y = 50;
+    private int diameter = 30;
+
+    // 构造函数初始化血量和x坐标
+    Blood(int x) {
+        this.blood = 3;
+        this.x = x;
+        this.color = Color.RED;
     }
 
-    public void bloodMinusOne(){
-        this.blood = this.blood-1;
+    // 血量减少并检查是否死亡
+    public void bloodMinusOne() {
+        this.blood -= 1;
+        if (this.blood == 0) {
+            System.out.println("died");
+        }
+        
     }
-    public int getBlood(){
+
+    // 获取当前血量
+    public int getBlood() {
         return this.blood;
+    }
+
+    // 绘制血量对象
+    public void draw(Graphics g) {
+        g.setColor(color);
+        g.fillOval(x, y, diameter, diameter);
+    }
+
+    // 更新颜色基于当前血量
+    public void eliminate() {
+        this.color = Color.GRAY;
     }
 }
