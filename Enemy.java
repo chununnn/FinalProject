251f5cc4c143d9d1b8033ab9bd133242a1c8344a
張正璽 <timchang0217@gmail.com>
2024-06-13 16:32:18 +0800

    hihi

--- Enemy.java
+++ Enemy.java
@@ -1,5 +1,33 @@
 import javax.swing.JPanel;
 
-public class Enemy {
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.util.Random;
 
+public class Enemy extends JPanel{
+    private static final int enemySize = 30;
+    Random random = new Random();
+    int x = random.nextInt(1600 - enemySize);
+    int y = enemySize;
+    int incy = 1; // 這是石頭要移動的單位
+    private Window window;
+
+    public Enemy(Window w) {
+        this.window = w;
+    }
+
+    void moveEnemy() {
+        if (y + incy > window.getHeight() - enemySize) {
+            x = random.nextInt(window.getWidth() - enemySize);
+            y = enemySize;
+        } else {
+            y += incy;
+        }
+
+    }
+
+    public void paint(Graphics2D g) {
+        g.setColor(Color.BLACK);
+        g.fillOval(x, y, enemySize, enemySize);
+    }
 }
--- Plane.java
+++ Plane.java
@@ -1,22 +1,47 @@
-import javax.swing.JPanel;
-import java.awt.Image;
 import javax.swing.ImageIcon;
-import javax.swing.JLabel;
+import javax.swing.JPanel;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.event.KeyEvent;
 
-public class Plane extends JPanel{
-    private JLabel label;
-    public Plane() {
-        ImageIcon imageTemp = new ImageIcon("12+ Fighter Jet Cartoon Png.jpg");
+public class Plane extends JPanel {
+    int planeWidth = 60; //飛機大小
+    int planeHeight = 80;
+    int x = (1600 - planeWidth) / 2; //飛機初始位置
+    int y = 1200 - planeHeight - 200;
+    private Window window;
+    private Image image;
 
-        int imageWdith = 135;
-        int imageHeight = 180;
+    public Plane(Window w) { // 建構子
+        this.window = w;
+        ImageIcon icon = new ImageIcon(getClass().getResource("12+ Fighter Jet Cartoon Png.jpg"));
+        image = icon.getImage();
+    }
 
-        Image scaledImage = imageTemp.getImage().getScaledInstance(imageWdith, imageHeight,Image.SCALE_SMOOTH);
-        ImageIcon image = new ImageIcon(scaledImage);
+    int xa = 0;
 
-        label = new JLabel(image);
-        label.setBounds((800 - imageWdith) / 2, 600 - imageHeight - 10, imageWdith, imageHeight);
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+            xa = -200;
+        } 
+        if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+            xa = 200;
+        } 
+    }
 
-        add(label);
+    public void keyReleased(KeyEvent e) {
+        xa = 0;
+    }
+
+    public void movePlane() {
+        if (x + xa < window.getWidth() - planeWidth && x + xa > 0) {
+            x += xa;
+        }
+    }
+
+    public void paint(Graphics2D g) {
+        if (image != null) {
+            g.drawImage(image, x, y, planeWidth, planeHeight, this);
+        }
     }
 }
