4a8005a052eea607eac49f6b975d0d47ae35ad52
AllenLai0923 <allen472561916@gmail.com>
2024-06-18 00:28:24 +0800

    Random stone size

--- Enemy.java
+++ Enemy.java
@@ -5,20 +5,22 @@ import java.awt.Rectangle;
 import java.util.Random;
 
 public class Enemy extends JPanel{
-    public final int enemySize = 30;
+    public int enemySize;
     Random random = new Random();
     int x = random.nextInt(720 - enemySize);
     int y = enemySize;
-    public int enemyblood = 5;
+    public int enemyblood;
     private long speed; // 這是石頭要移動的單位
     private Window window;
     private int index;
     public boolean visiable = true;
 
     public Enemy(Window w, int index, long speed) {
+        this.enemySize = 10 * random.nextInt(10) + 30;
         this.index = index;
         this.window = w;
         this.speed = speed;
+        this.enemyblood = enemySize / 25 + 3;
     }
 
     public int EBloodMinusOne() {
